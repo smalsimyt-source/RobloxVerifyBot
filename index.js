345c033f2509bc9577c9d2c8a1a0cdc7ecbbe17c
smalsimyt-source <smalsimyt@gmail.com>
2026-07-28 15:12:54 +0200

    package.json
    {
      "name": "robloxverifybot",
      "version": "1.0.0",
      "main": "index.js",
      "scripts": {
        "start": "node index.js"
      },
      "dependencies": {
        "discord.js": "^14.0.0"
      }
    }

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`Bot działa jako ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
